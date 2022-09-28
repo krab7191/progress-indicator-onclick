43355c06e3d3ae35952a047072a369ccf5077787
Karsten Rabe <karstenrabe91@gmail.com>
2022-09-28 10:46:16 -0400

    Fix issue by removing onClick handler, and add onChange handler to parent

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.scss';
 import styles from './App.module.scss';
 import { useEffect, useState } from 'react';
 
-const list = [
-  'One',
-  'Two',
-  'Three',
-  'Four'
+const initialList = [
+  '1',
+  '2',
+  '3',
+  '4'
 ];
 
 function App() {
-  const [clicked, setClicked] = useState('Not clicked!');
   const [err, setErr] = useState('');
+  const [list, setList] = useState(initialList);
 
   useEffect(() => {
     window.onerror = function(e){
@@ -31,6 +31,17 @@ function App() {
     return () => clearTimeout(timer);
   }, [err]);
 
+  const changeHandler = (idx) => {
+    const newArr = list.slice(0, idx+1);
+    setList(newArr);
+  };
+
+  const addHandler = () => {
+    const newList = [ ...list ];
+    newList.push(list.length+1);
+    setList(newList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -38,22 +49,19 @@ function App() {
       </header>
       <p>INSTRUCTIONS: click a progress step on the left. Nothing will happen even though onClick prop is present. Highlight a step and press the enter key. This results in an error.</p>
       <br />
-      <p>{clicked}</p>
       <br />
       <div id='error' className={styles.error}>{err}</div>
       <div className={styles.container}>
-        <ProgressIndicator vertical currentIndex={3} style={{ position: 'fixed' }}>
+        <ProgressIndicator vertical currentIndex={list.length -1} style={{ position: 'fixed' }} onChange={changeHandler}>
           {list.map((name, idx) => (
             <ProgressStep
               key={`${name}-${idx}`}
               label={name}
-              secondaryLabel={idx === 2 ? list[idx] : null}
-              onClick={() => {
-                setClicked('Clicked!');
-              }}
+              secondaryLabel={idx === list.length - 1 ? list[idx] + '' : null}
             />
           ))}
         </ProgressIndicator>
+        <button onClick={addHandler}>Add</button>
       </div>
     </div>
   );
